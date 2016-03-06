0843d4b571124da5018692dbbbace3cad66c22de
Dan Hough <daniel.hough@gmail.com>
2016-03-06 15:03:11 +0000

    Placeholder then see what you picked

--- bin/bundle.js
+++ bin/bundle.js
@@ -47,18 +47,24 @@ var instructionPanel = document.getElementById('instructions');
 var resultsPanel = document.getElementById('resultsPanel');
 var tryAgainButton = document.getElementById('tryAgain');
 var resultsSummary = document.getElementById('resultsSummary');
+var recallChoices = document.getElementById('recallChoices');
+var recallSelection = document.getElementById('recallSelection');
+var currentTestPanel = document.getElementById('currentTestPanel');
 
 var $startRecallForm = bonzo(startRecallForm);
 var $instructionPanel = bonzo(instructionPanel);
 var $resultsPanel = bonzo(resultsPanel);
 var $resultsSummary = bonzo(resultsSummary);
+var $recallChoices = bonzo(recallChoices);
+var $recallSelection = bonzo(recallSelection);
+var $currentTestPanel = bonzo(currentTestPanel);
 
 function randomlySelectFromArray(ar) {
 	var item = items[Math.floor(Math.random() * ar.length)];
 }
 
 function resetUI() {
-	recallCount = 0;
+	recallCount = 1;
 	$resultsPanel.addClass('hidden');
 	$instructionPanel.addClass('hidden');
 	$startRecallForm.removeClass('hidden');
@@ -74,33 +80,64 @@ function showGameOver() {
 	tryAgainButton.addEventListener('click', resetUI, false);
 	$instructionPanel.addClass('hidden');
 	$resultsPanel.removeClass('hidden');
+	$currentTestPanel.addClass('hidden');
+	$recallChoices.empty();
+	$recallSelection.empty();
 }
 
-function verifyChoices(choices, stage) {
+function addSelection(emojiText) {
+	var $selectionSpan = bonzo(recallSelection.children).first();
+	currentAnswer.push(emojiText);
+
+	while (!$selectionSpan.hasClass('emoji-selection-empty')) {
+		$selectionSpan = $selectionSpan.next();
+	}
+
+	$selectionSpan.removeClass('emoji-selection-empty');
+	$selectionSpan.addClass('emoji-selection');
+	$selectionSpan.text(emojiText);
+}
+
+function verifyChoices(stage) {
+	if (currentAnswer.length === recallCount) {
+		if (difference(currentAnswer, stage).length === 0) {
+			alert('Correct!');
+			recallCount += 1;
+			nextStage();
+		} else {
+			alert('Nope. You were shown:\n' + stage.join(' ') + '\nBut your answer was:\n' + currentAnswer.join(' ') + '\n\nBetter luck next time.');
+			showGameOver();
+		}
+	}
+}
+
+function showChoices(choices, stage) {
 	var m = 0;
-	var heading = "<h2>These were the choices. Click to put them in the text box.</h2>"
-	$instructionPanel.html(heading);
-	var choiceButton;
+	var b = 0;
+	var choiceButton, emptySelection;
+
+	for (b = 0; b < recallCount; b++) {
+		emptySelection = document.createElement('span');
+		bonzo(emptySelection).addClass('emoji emoji-selection-empty');
+		bonzo(emptySelection).text('?');
+		$recallSelection.append(emptySelection);
+	}
+
 	for (m = choices.length - 1; m >= 0; m--) {
 		choiceButton = document.createElement('a');
 		bonzo(choiceButton).addClass('emoji emoji-choice');
 		bonzo(choiceButton).text(choices[m]);
 		// TODO: Unbind these puppies
 		choiceButton.addEventListener('click', function(event) {
-			currentAnswer.push(bonzo(this).text());
-			if (currentAnswer.length === recallCount) {
-				if (difference(currentAnswer, stage).length === 0) {
-					alert('Correct!');
-					recallCount += 1;
-					nextStage();
-				} else {
-					alert('Nope. You were shown:\n' + stage.join(' ') + '\nBut your answer was:\n' + currentAnswer.join(' ') + '\n\nBetter luck next time.');
-					showGameOver();
-				}
-			}
+			var emojiText = bonzo(this).text();
+			addSelection(emojiText);
+			verifyChoices(stage);
 		}, false);
-		$instructionPanel.append(choiceButton);
+		$recallChoices.append(choiceButton);
 	}
+
+	$currentTestPanel.removeClass('hidden');
+	$instructionPanel.addClass('hidden');
 }
 
 function nextStage() {
@@ -109,7 +146,13 @@ function nextStage() {
 	var stage = sampleSize(choices, recallCount);
 	currentAnswer = [];
 	var m = 0;
+
+	// Reset for the next level
+	$currentTestPanel.addClass('hidden');
+	$recallChoices.empty();
+	$recallSelection.empty();
 	$startRecallForm.addClass('hidden');
+
 	delay(showInstructionText, 0, '3');
 	delay(showInstructionText, 1000, '2');
 	delay(showInstructionText, 2000, '1');
@@ -118,7 +161,7 @@ function nextStage() {
 		delay(showInstructionText, 3000 + (m * 1000), stage[m]);
 	}
 	delay(showInstructionText, 3000 + (m++ * 1000), 'Done.');
-	delay(verifyChoices, 3000 + (m * 1000), choices, stage);
+	delay(showChoices, 3000 + (m * 1000), choices, stage);
 }
 
 tests = [];
@@ -132,7 +175,7 @@ function debug() {
 	var numberOfChoices = Math.min(recallCount * 5, allEmoji.length);
 	var choices = sampleSize(allEmoji, numberOfChoices);
 	var stage = sampleSize(choices, recallCount);
-	verifyChoices(choices, stage);
+	showChoices(choices, stage);
 }
 
 if (!isEmojiSupported(thinkingEmoji)) {
